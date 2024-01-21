8f0a0be3e687efb46c83170f737285c0e9073338
ben.watt <ben.watt@moonpig.com>
2024-01-21 20:55:11 +0000

    Add additional dependencines for tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,16 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <input
-      type="text"
-      className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"
-      placeholder="This is placeholder"
-    />
+    <>
+      <h1 className="text-3xl font-bold underline text-red-500">
+        Hello world!
+      </h1>
+      <input
+        type="text"
+        className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"
+        placeholder="This is placeholder"
+      />
+    </>
+  
   )
 }
