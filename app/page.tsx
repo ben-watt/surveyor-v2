cce4d04020e244d7033d30d702364c1ce31e938c
ben.watt <ben.watt@moonpig.com>
2024-01-22 21:03:08 +0000

    Add some base level functionality with pre-line to ensure we're setup

--- app/page.tsx
+++ app/page.tsx
@@ -5,15 +5,11 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-red-500">
-        Hello world!
-      </h1>
-      <input
-        type="text"
-        className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"
-        placeholder="This is placeholder"
-      />
+      <div className="container mx-auto px-5 xl:px-40">
+        <h1 className="text-4xl dark:text-white">Building Survey Report</h1>
+          <input type="text" className="my-4 py-3 px-4 block w-full border border-gray-900 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" placeholder="This is placeholder" />
+          <input type="text" className="py-3 px-4 block w-full border border-gray-900 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" placeholder="This is placeholder" />
+      </div>
     </>
-  
   )
 }
