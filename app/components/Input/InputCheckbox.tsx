ddc3e031b4a9ba979979174bf3a094dbd8024cf9
ben.watt <ben.watt@moonpig.com>
2024-08-15 10:46:55 +0100

    Update input checkbox

--- app/components/Input/InputCheckbox.tsx
+++ app/components/Input/InputCheckbox.tsx
@@ -1,25 +1,36 @@
-import { Checkbox } from "@/components/ui/checkbox"
-import { FieldValues, Path, useController, UseControllerProps } from "react-hook-form";
-import { v4 } from "uuid"
+import { Checkbox } from "@/components/ui/checkbox";
+import { useEffect, createRef } from "react";
+import {
+  FieldValues,
+  Path,
+  useController,
+  UseControllerProps,
+} from "react-hook-form";
+import { v4 } from "uuid";
 
 interface InputCheckboxProps {
-    labelText? : string
-    rhfName: Path<FieldValues>
-    controllerProps?: Omit<UseControllerProps<FieldValues>, "name">
+  labelText?: string;
+  rhfName: Path<FieldValues>;
+  controllerProps?: Omit<UseControllerProps<FieldValues>, "name">;
 }
 
-export function InputCheckbox({ rhfName, labelText, controllerProps }: InputCheckboxProps) {
-  const id = v4();
+export function InputCheckbox({
+  rhfName,
+  labelText,
+  controllerProps,
+}: InputCheckboxProps) {
+  const id = "checkbox-" + v4().slice(0, 8);
   const { field } = useController({ name: rhfName, ...controllerProps });
 
   return (
-    <div className="flex items-center space-x-2">
-      <Checkbox id={id} checked={field.value} onCheckedChange={field.onChange} {...field} />
-      <label
-        htmlFor={id}
-        className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
-        {labelText}
-      </label>
+    <div className="flex items-center space-x-2" >
+        <Checkbox id={id} checked={field.value} onCheckedChange={field.onChange} {...field}/>
+        <label
+          htmlFor={id}
+          className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+        >
+          {labelText}
+        </label>
     </div>
-  )
+  );
 }
