aa39c80f6d38625a19aa5b2fb3afd88d721dbad6
ben.watt <ben.watt@moonpig.com>
2024-08-15 11:08:25 +0100

    Update checkbox props

--- app/components/Input/InputCheckbox.tsx
+++ app/components/Input/InputCheckbox.tsx
@@ -23,7 +23,7 @@ export function InputCheckbox({
 
   return (
     <div className="flex items-center space-x-2" >
-        <Checkbox id={id} />
+        <Checkbox id={id} checked={field.value} onCheckedChange={field.onChange} {...field}/>
         <label
           htmlFor={id}
           className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
