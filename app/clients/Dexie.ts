f1ca359a75080923253e7b3fe8ce8f72d138b803
ben.watt <ben.watt@moonpig.com>
2024-08-25 15:22:08 +0100

    add dixey cloud

--- app/clients/Dexie.ts
+++ app/clients/Dexie.ts
@@ -1,5 +1,7 @@
 import Dexie, { type EntityTable } from 'dexie';
 import { type Schema } from "@/amplify/data/resource";
+import dexieCloud from "dexie-cloud-addon";
+import dixycloudjson from "@/dexie-cloud.json";
 
 export type Survey = Omit<Schema['Surveys']['type'], "createdAt" | "updatedAt" | "owner"> & { "createdAt"?: string, "updatedAt"?: string };
 
@@ -20,4 +22,24 @@ db.version(1).stores({
   surveys: 'id'
 });
 
-export { db };
+/* New version of the database */
+
+export type Sureyv2 = {
+  id: string;
+  content: string;
+}
+
+const dbV2 = new Dexie('SurveyorApp', { addons: [dexieCloud] }) as Dexie & {
+  surveys: EntityTable<Sureyv2>;
+}
+
+dbV2.version(1).stores({
+  surveys: 'id, owner'
+});
+
+dbV2.cloud.configure({
+  databaseUrl: dixycloudjson.dbUrl,
+  requireAuth: true,
+})
+
+export { db, dbV2 };
--- app/innerLayout.tsx
+++ app/innerLayout.tsx
@@ -7,12 +7,30 @@ import "instantsearch.css/themes/satellite.css";
 import { Authenticator } from "@aws-amplify/ui-react";
 import { Toaster } from "react-hot-toast";
 import SecureNav from "./components/Navbar";
+import { dbV2 } from "./clients/Dexie";
+import { useEffect } from "react";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+
+  useEffect(() => {
+    async function init() {
+      dbV2.cloud.login()
+      const user = dbV2.cloud.currentUser
+      
+      dbV2.surveys.toArray().then((survey) => {
+        console.log("[DBV2] Survey", survey);
+      });
+    
+      console.log("[DBV2] Current User", user);
+    }
+
+    init();
+  })
+
   return (
     <>
         <ConfigureAmplifyClientSide />
