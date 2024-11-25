ced7afcca9d4fe535eab5bc5eee51f7bd92ec867
ben.watt <ben.watt@moonpig.com>
2024-11-25 14:38:05 +0000

    Fix formatting of Content section

--- app/surveys/building-survey-reports/BuildingSurveyReportTipTap.tsx
+++ app/surveys/building-survey-reports/BuildingSurveyReportTipTap.tsx
@@ -214,7 +214,7 @@ export default function PDF({ form }: PdfProps) {
       </Page>
       <Page>
         <p
-          style={{ fontSize: "1.5em", marginBottom: "8mm", fontWeight: "bold" }}
+          style={{ fontSize: "1.5em", marginBottom: "8mm" }}
         >
           Contents
         </p>
