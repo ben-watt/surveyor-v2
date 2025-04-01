13f89846616c05a813aac8d72623a39db97f435a
ben.watt <ben.watt@moonpig.com>
2025-04-01 21:12:11 +0100

    Add back auth

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -5,12 +5,13 @@ import { storage } from './storage/resource.js';
 import { CloudwatchRum } from "./custom/rum/resource";
 
 const backend = defineBackend({
+  auth,
   data,
   storage,
 });
 
-// new CloudwatchRum(backend.createStack("cloudWatchRum"), "cloudWatchRum", {
-//   guestRole: backend.auth.resources.unauthenticatedUserIamRole,
-//   identityPoolId: backend.auth.resources.cfnResources.cfnIdentityPool.attrId,
-//   domain: process.env.CLOUDWATCH_RUM_DOMAIN || "localhost",
-// });
+new CloudwatchRum(backend.createStack("cloudWatchRum"), "cloudWatchRum", {
+  guestRole: backend.auth.resources.unauthenticatedUserIamRole,
+  identityPoolId: backend.auth.resources.cfnResources.cfnIdentityPool.attrId,
+  domain: process.env.CLOUDWATCH_RUM_DOMAIN || "localhost",
+});
