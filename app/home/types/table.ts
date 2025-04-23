508b1f3610da77edaeed0afd2f9ae40bc2aa449d
ben.watt <ben.watt@moonpig.com>
2025-04-23 10:54:23 +0100

    fix linting errors

---
+++ app/home/types/table.ts
@@ -0,0 +1,12 @@
+import { ColumnDef, ColumnMeta } from '@tanstack/react-table';
+
+export interface TableColumnMeta extends ColumnMeta<any, any> {
+  tw?: {
+    headerClassName?: string;
+    cellClassName?: string;
+  };
+}
+
+export type TableColumnDef<TData> = ColumnDef<TData, any> & {
+  meta?: TableColumnMeta;
+}; 
