36491f7e6fb7aac92e9fdcff6384454c591eb4d4
ben.watt <ben.watt@moonpig.com>
2025-04-26 22:39:51 +0100

    Add the ability to highlight text - allows changing of the statuses

--- app/home/components/Input/BlockMenuBar.tsx
+++ app/home/components/Input/BlockMenuBar.tsx
@@ -33,6 +33,8 @@ import {
   TableOfContents,
   Image,
   ImagePlus,
+  Brush,
+  Highlighter,
 } from "lucide-react";
 import {
   Select,
@@ -44,6 +46,14 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { Level } from "@tiptap/extension-heading";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { useLocalStorage } from "@uidotdev/usehooks";
 
 interface MenuBarProps {
   editor: Editor | null;
@@ -81,6 +91,10 @@ export default function MenuBar({ editor, onPrint }: MenuBarProps) {
       title: "Code",
       action: () => editor.chain().focus().toggleCode().run(),
       isActive: () => editor.isActive("code"),
+    },
+    {
+      type: "highlight",
+      render: () => <HighlightColorPicker editor={editor} />,
     },
     {
       icon: <RemoveFormatting />,
@@ -161,10 +175,14 @@ export default function MenuBar({ editor, onPrint }: MenuBarProps) {
         file.onchange = (e) => {
           const file = (e.target as HTMLInputElement).files?.[0];
           if (file) {
-            editor.chain().focus().setImage({
-              src: URL.createObjectURL(file),
-              alt: "Image",
-            }).run();
+            editor
+              .chain()
+              .focus()
+              .setImage({
+                src: URL.createObjectURL(file),
+                alt: "Image",
+              })
+              .run();
           }
         };
         file.click();
@@ -374,21 +392,21 @@ const MenuFontSize = ({ editor }: MenuFontSizeProps) => {
       const setFromNode = (nodeType: string) => {
         const fontSize = editor.getAttributes(nodeType).fontSize as string;
         const size = parseInt(fontSize.replace("pt", ""));
-        if(size) {
+        if (size) {
           setFontSize(size);
         } else {
           setFontSize(DEFAULT_FONT_SIZE);
         }
-      }
+      };
 
-      if(editor.isActive("heading")) {
+      if (editor.isActive("heading")) {
         setFromNode("heading");
       }
 
-      if(editor.isActive("paragraph")) {
+      if (editor.isActive("paragraph")) {
         setFromNode("paragraph");
       }
-      
+
       if (editor.isActive("textStyle")) {
         setFromNode("textStyle");
       }
@@ -423,13 +441,22 @@ const MenuFontSize = ({ editor }: MenuFontSizeProps) => {
         <Minus onClick={() => reduceFontSize()} />
       </button>
       <div className="ml-1 mr-1">
-        <Select value={fontSize.toString()} onValueChange={v => changeFontSize(v)}>
+        <Select
+          value={fontSize.toString()}
+          onValueChange={(v) => changeFontSize(v)}
+        >
           <SelectTrigger className="[&_svg]:hidden">
             <SelectValue />
           </SelectTrigger>
           <SelectContent className="min-w-11">
             <SelectGroup>
-              {Array.from(new Set(fontSizeDropdown.concat(fontSize))).map((size) => (<SelectItem key={size} value={size.toString()}>{size}</SelectItem>))}
+              {Array.from(new Set(fontSizeDropdown.concat(fontSize))).map(
+                (size) => (
+                  <SelectItem key={size} value={size.toString()}>
+                    {size}
+                  </SelectItem>
+                )
+              )}
             </SelectGroup>
           </SelectContent>
         </Select>
@@ -438,5 +465,89 @@ const MenuFontSize = ({ editor }: MenuFontSizeProps) => {
         <Plus onClick={() => increaseFontSize()} />
       </button>
     </div>
+  );
+};
+
+const HighlightColorPicker = ({ editor }: { editor: Editor }) => {
+  const [selectedColour, setSelectedColour] = useLocalStorage<string>(
+    "HighlightColourPicker_selectedColour",
+    "#ff0000"
+  );
+  const [colourPickerOpen, setColourPickerOpen] = useState<boolean>(false);
+  const colours = [
+    { name: "Red", value: "#ff0000" },
+    { name: "Yellow", value: "#ffff00" },
+    { name: "Green", value: "#00ff00" },
+    { name: "Blue", value: "#0000ff" },
+    { name: "Pink", value: "#ffc0cb" },
+    { name: "Orange", value: "#ffa500" },
+  ];
+
+  const toggleHighlight = (colour: string) => {
+    setSelectedColour(colour);
+    editor.chain().focus().toggleHighlight({ color: colour }).run();
+    setColourPickerOpen(false);
+  };
+
+  const handleIconClick = () => {
+    if (!editor.state.selection.empty) {
+      editor.chain().focus().toggleHighlight({ color: selectedColour }).run();
+    } else {
+      setColourPickerOpen(true);
+    }
+  };
+
+  return (
+    <div className="relative">
+      <Button
+        variant="ghost"
+        size="sm"
+        onClick={() => handleIconClick()}
+        style={{ borderColor: selectedColour }}
+        className={cn(
+          "w-8 p-0 h-full",
+          editor.isActive("highlight") && "bg-muted"
+        )}
+      >
+        <HighlighterSvg nibColour={selectedColour} />
+      </Button>
+      {colourPickerOpen && (
+        <div className="grid grid-rows-1 gap-2 absolute top-0 left-0 bg-white border p-2 rounded-md">
+          {colours.map((colour) => (
+            <button
+              key={colour.value}
+              className={cn(
+                "h-6 w-6 rounded-full border border-border",
+                selectedColour === colour.value &&
+                  "ring-2 ring-offset-2 ring-offset-background ring-ring"
+              )}
+              style={{ backgroundColor: colour.value }}
+              onClick={() => toggleHighlight(colour.value)}
+              title={colour.name}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+const HighlighterSvg = ({ nibColour }: { nibColour: string }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      className="lucide lucide-highlighter-icon lucide-highlighter"
+    >
+      <path style={{ fill: nibColour }} d="m9 11-6 6v3h9l3-3" />
+      <path  d="m22 12-4.6 4.6a2 2 0 0 1-2.8 0l-5.2-5.2a2 2 0 0 1 0-2.8L14 4" />
+    </svg>
   );
 };
